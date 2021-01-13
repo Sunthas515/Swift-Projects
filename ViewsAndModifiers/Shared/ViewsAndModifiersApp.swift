78f8f034e9110854e11d7a6c6e7f4aebf3ba8147
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-01-13 10:26:28 +1000

    Add files via upload

---
+++ ViewsAndModifiers/Shared/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  Shared
+//
+//  Created by Callum McNeilage on 17/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
