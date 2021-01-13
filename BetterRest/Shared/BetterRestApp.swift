9a36be32dc0e7aee85ebd79e3b939df65041082f
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-01-13 10:27:52 +1000

    Add files via upload

---
+++ BetterRest/Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Callum McNeilage on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
