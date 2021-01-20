0384f96a92e8f1e5da71b111032552f0744f899b
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2021-01-20 15:04:47 +1000

    Added animations project

---
+++ Animations/Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by Callum McNeilage on 20/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
